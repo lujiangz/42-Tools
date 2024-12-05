80f3780163612bbbfd313e20b9c7edd21eb16a52
lujiangz <memocaps12@gmail.com>
2024-12-05 14:29:14 +0300

    finished?

---
+++ 42-devstation/start.sh
@@ -0,0 +1,15 @@
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided. ./start <SHARED_FOLDER>"
+
+docker run \
+  --cap-drop=ALL  \
+  --cap-add=SYS_PTRACE \
+  --privileged \
+  --security-opt seccomp=unconfined \
+  --security-opt apparmor=unconfined \
+  --rm \
+  -it -v $1:/root/shared luji/devstation zsh
